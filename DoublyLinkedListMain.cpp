578a1341ffe70376f701d1c2c71344736a5aa7eb
Mathew Lister <36172965+AngryPorkchop11@users.noreply.github.com>
2019-07-31 16:50:22 -0700

    Add files via upload

--- DoublyLinkedListMain.cpp
+++ DoublyLinkedListMain.cpp
@@ -1,30 +1,30 @@
-///////////////////////////////////////
-////////////DLL CHECK///////////////
 /////////////////////////////////////
-//#include <iostream> 
-//#include <string>
-//#include "DoublyLinkedList.h"
-//
-//using namespace std;
-//
-//int main()
-//{
-//	int key;
-//	cout << "enter -1 to exit & print \n";
-//	cout << "Enter Number to add: ";
-//	cin >> key;
-//	cout << endl;
-//
-//	DoublyLinkedList<int> *list = new DoublyLinkedList<int>();
-//	while (key != -1)
-//	{
-//		list->AppendItemO1(key);
-//		cout << endl;
-//		cout << "Enter Number to add: ";
-//		cin >> key;
-//	}
-//	cout << "-----------------------------------------------\n";
-//	cout << "List: ";
-//	list->print();
-//	return 0;
-//}
+//////////DLL CHECK///////////////
+///////////////////////////////////
+#include <iostream> 
+#include <string>
+#include "DoublyLinkedList.h"
+
+using namespace std;
+
+int main()
+{
+	int key;
+	cout << "enter -1 to exit & print \n";
+	cout << "Enter Number to add: ";
+	cin >> key;
+	cout << endl;
+
+	DoublyLinkedList<int> *list = new DoublyLinkedList<int>();
+	while (key != -1)
+	{
+		list->AppendItemO1(key);
+		cout << endl;
+		cout << "Enter Number to add: ";
+		cin >> key;
+	}
+	cout << "-----------------------------------------------\n";
+	cout << "List: ";
+	list->print();
+	return 0;
+}
